7cf216c07aa1af91bd272eba82af93e98032d4eb
camwalford <cwalford2012@gmail.com>
2023-04-05 17:37:56 -0700

    Added additional flags to gcc in script

--- script.bash
+++ script.bash
@@ -34,7 +34,7 @@ fi
 for file in $c_files
 do
   echo "Compiling $file..."
-  gcc -o "${file%.c}" "$file" >> "$times_file" 2>&1 || { echo "Error compiling $file" ; exit 1 ; }
+  gcc -O3 -march=native -flto -funroll-loops -o "${file%.c}" "$file" >> "$times_file" 2>&1 || { echo "Error compiling $file" ; exit 1 ; }
 done
 
 # Run each compiled file with the specified arguments, log the times, and diff the output files
